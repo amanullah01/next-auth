bcd14799aaac6c4726b6d5e4f4f375e308657388
Amanullah Aman <amancse.aiub@gmail.com>
2023-09-09 01:01:20 +0600

    authentication logout

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -2,14 +2,17 @@ import Link from "next/link";
 import { useSession, signOut } from "next-auth/react";
 
 import classes from "./main-navigation.module.css";
+import { useRouter } from "next/router";
 
 function MainNavigation() {
+  const router = useRouter();
   const { data: session, status } = useSession();
   console.log(status);
   console.log(session);
 
-  const logoutHandler = () => {
-    signOut();
+  const logoutHandler = async () => {
+    const data = await signOut({ redirect: false, callbackUrl: "/auth" });
+    router.push(data.url);
   };
 
   return (
