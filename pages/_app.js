567a1759a9b6d4cc59a6907ede5a9c81d69f699b
Amanullah Aman <amancse.aiub@gmail.com>
2023-09-09 02:16:55 +0600

    Password update api done

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import { SessionProvider } from "next-auth/react";
 import Layout from "../components/layout/layout";
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps: session, ...pageProps }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
       <Layout>
---
+++ pages/api/user/change-password.js
@@ -0,0 +1,52 @@
+import { hashPassword, verifyPassword } from "@/lib/auth";
+import { connectToDatabase } from "@/lib/db";
+import { getSession } from "next-auth/react";
+
+const handler = async (req, res) => {
+  if (req.method !== "PATCH") {
+    return;
+  }
+
+  const session = await getSession({ req: req });
+
+  if (!session) {
+    res.status(401).json({ message: "Not authenticated!" });
+    return;
+  }
+
+  const userEmail = session.user.email;
+  const oldPassword = req.body.oldPassword;
+  const newPassword = req.body.newPassword;
+
+  const client = await connectToDatabase();
+  const userCollection = client.db().collection("users");
+  const user = await userCollection.findOne({ email: userEmail });
+
+  if (!user) {
+    res.status(404).json({ message: "user not found" });
+    client.close();
+    return;
+  }
+
+  const currentPassword = user.password;
+  const isPasswordEqual = await verifyPassword(oldPassword, currentPassword);
+
+  if (!isPasswordEqual) {
+    res.status(403).json({ message: "Password is not same" });
+    client.close();
+    return;
+  }
+
+  const hasedPass = hashPassword(newPassword);
+
+  const result = await userCollection.updateOne(
+    { email: userEmail },
+    { $set: { password: hasedPass } }
+  );
+
+  client.close();
+
+  res.status(200).json({ message: "Password updated!" });
+};
+
+export default handler;
