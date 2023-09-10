5c50971936796a3c6a1b9e1eeac17f91c846c424
Amanullah Aman <amancse.aiub@gmail.com>
2023-09-10 21:10:44 +0600

    hello route secured

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { getServerSession } from "next-auth";
+import { authOptions } from "./auth/[...nextauth]";
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
+  const session = await getServerSession(req, res, authOptions);
+  console.log("api session");
+  console.log(session);
+
+  if (!session) {
+    res.status(401).json({ message: "Not authenticated!" });
+    return;
+  }
+
+
   res.status(200).json({ name: 'John Doe' })
 }
